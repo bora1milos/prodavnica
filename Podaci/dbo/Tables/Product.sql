61e24a41a8593cabfc1d4e3755188f328285f532
Borislav <bora1milos@yahoo.com>
2021-02-17 20:42:06 +0100

    Added register database.

---
+++ Podaci/dbo/Tables/Product.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Product]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY,
+    [ProductName] NVARCHAR(50) NOT NULL,
+    [Descrption] NVARCHAR(MAX) NOT NULL,
+    [RetialPrice] MONEY NOT NULL,
+    [CreateDate] DATETIME2 NOT NULL DEFAULT getutcdate(),
+    [LastModified] DATETIME2 NOT NULL DEFAULT getutcdate()
+)
